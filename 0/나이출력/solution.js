6de52cd46fdc31cacd27a2c1d8c36bfc5329585f
Plush777 <sky11916@naver.com>
2023-10-31 17:45:33 +0900

    231031

---
+++ 0/나이출력/solution.js
@@ -0,0 +1,9 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120820
+
+function solution(age) {
+    var year = 2023;
+    
+    var answer = year - age;
+    
+    return answer;
+}
